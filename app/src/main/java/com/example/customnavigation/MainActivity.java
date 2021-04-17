77225712e5987e708fd0b447ac6c4752da46894e
PC <ngkhanhnguyen09@gmail.com>
2021-04-17 14:52:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/customnavigation/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.customnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    Button Inside_Navigation,Outside_Navigation,Open_Menu;
+    DrawerLayout drawable;
+    NavigationView navigationView;
+    androidx.appcompat.widget.Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Anhxa();
+        open_menu();
+    }
+
+
+
+    public  void Anhxa()
+    {
+        Inside_Navigation=(Button)findViewById(R.id.Inside_Navigation);
+        Outside_Navigation=(Button)findViewById(R.id.Outside_Navigation);
+        Open_Menu=(Button)findViewById(R.id.open_menu);
+        drawable=(DrawerLayout) findViewById(R.id.DrawerLayout);
+        navigationView=(NavigationView)findViewById(R.id.NavigationView);
+        toolbar= (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+
+
+
+    public void  open_menu()
+    {
+     Open_Menu.setOnClickListener(new View.OnClickListener() {
+         @SuppressLint("WrongConstant")
+         @Override
+         public void onClick(View v) {
+             drawable.openDrawer(Gravity.START);
+         }
+     });
+    }
+
+}
